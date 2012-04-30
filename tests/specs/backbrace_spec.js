e66003a4f73df4fc6f740f9cd0a6f63e7cccdeea
Patrick Williams <pwmckenna@gmail.com>
2012-04-30 15:05:03 -0700

    adding tests for collections

--- tests/specs/backbrace_spec.js
+++ tests/specs/backbrace_spec.js
@@ -36,6 +36,34 @@
 				expect(callback).toHaveBeenCalled();
 			});
 		});
+		it('calls a callback for a static selector containing models and collections that are added after live call', function() {
+			runs(function() {
+				var callback = jasmine.createSpy();
+				var model = new Backbone.Model;
+				model.live('a b c d', callback);
+
+				model.set('a', new Backbone.Collection);
+				model.get('a').add(new Backbone.Model({id: 'b'}));
+				model.get('a').get('b').set('c', new Backbone.Collection);
+				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
+
+				expect(callback).toHaveBeenCalled();
+			});
+		});
+		it('calls a callback for a static selector containing models and collections that are added before live call', function() {
+			runs(function() {
+				var callback = jasmine.createSpy();
+				var model = new Backbone.Model;
+
+				model.set('a', new Backbone.Collection);
+				model.get('a').add(new Backbone.Model({id: 'b'}));
+				model.get('a').get('b').set('c', new Backbone.Collection);
+				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
+
+				model.live('a b c d', callback);
+				expect(callback).toHaveBeenCalled();
+			});
+		});
 		it('does not call a callback when not all models are added before live call', function() {
 			runs(function() {
 				var callback = jasmine.createSpy();
