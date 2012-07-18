f65cd30215f3c0d078113466f2b2b5f100874b77
Patrick Williams <patrick@bittorrent.com>
2012-07-17 17:46:29 -0700

    re-implementing live

--- backbrace.js
+++ backbrace.js
@@ -6,119 +6,137 @@
 // http://pwmckenna.github.com/backbrace
 
 (function() {
-    function copy_and_unshift(array, item) {
-        var ret = _.toArray(array);
-        ret.unshift(item);
-        return ret;
-    }
-    // Backbone.Model specific functionality
-    function handle_existing_attributes(selector, callback, context, matching) {
-        if(selector === '*') {
-            _(this.toJSON()).each(function(value, key) {
-                handle_existing_attributes.call(this, key,  callback, context, matching);
-            }, this);
-            return;
-        }
-
-        if(this.has(selector)) {
-            callback.call(context, copy_and_unshift(matching, this.get(selector)));
-        }
-    }
-    function handle_future_attributes(selector, callback, context, matching) {
-        if(selector === '*') {
-            this.on('change', function() {
-                var prev = this.previousAttributes();
-                _(this.changedAttributes()).each(function(value, key) {
-                    if(!(key in prev)) {
-                        callback.call(context, copy_and_unshift(matching, this.get(key)));
-                    }
-                }, this);
-            }, this);
-        } else {
-            this.on('change:' + selector, function() {
-                var prev = this.previousAttributes();
-                if(!(selector in prev)) {
-                    callback.call(context, copy_and_unshift(matching, this.get(selector)));
-                }
-            }, this);
-        }
-    }
-    function handle_model(selector, callback, context, matching) {
-        handle_existing_attributes.call(this, selector,  callback, context, matching);
-        handle_future_attributes.call(this, selector,  callback, context, matching);
+    /**
+        @param rest - the attribute keys/model ids left to match
+        @param callback - the final callback that the user provided
+        @param context - the context to be provided for the user provided callback
+        @param matching - the values that have matched thus far
+        @param obj - the value that matched our previous "first" key
+    **/
+    function intermediate_callback(rest, callback, context, matching, obj) {
+        var m = _.clone(matching || []);
+        m.unshift(obj);
+        rest && obj && typeof obj.live !== 'undefined' && obj.live(rest, callback, context, m);
+        !rest && callback.apply(context, m);
     }
 
-    // Backbone.Collection specific functionality
-    function handle_existing_models(selector,  callback, context, matching) {
-        if(selector === '*') {
-            this.each(function(model) {
-                handle_existing_models.call(this, model.id,  callback, context, matching);
-            }, this);
-            return;
-        }
+    _.extend(Backbone.Collection.prototype, {
+        /**
+            @param selectors - a space delimited string of attribute keys/model ids to match
+            example: 'torrent * file * properties streaming_url'
+            @param callback - the callback to be called with the matching values, in reverse order
+            example: live('torrent * file * properties streaming_url', function(streaming_url, properties, file, file_list...)...
+            @param context - the this pointer in the callback
+            @param matching - not for external use...it is used to collect the callback arguments
+        **/
+        live: function(selectors, callback, context, matching) {
+            var _this = this;
+            var tokens = selectors.split(' ');
 
-        if(this.get(selector)) {
-            callback.call(context, copy_and_unshift(matching, this.get(selector)));
-        }
-    }
-    function handle_future_models(selector, callback, context, matching) {
-        this.on('add', function(elem) {
-            if(selector === '*' || selector === elem.id) {
-                callback.call(context, copy_and_unshift(matching, elem));
+            var first = null;
+            if(tokens.length > 0) {
+                first = _.first(tokens);
             }
-        }, this);
-    }
-    function handle_collection(selector, callback, context, matching) {
-        handle_existing_models.call(this, selector,  callback, context, matching);
-        handle_future_models.call(this, selector,  callback, context, matching);
-    }
 
-    // Generic functionality for Backbone Models/Collections
-    // The 'live' function should be the only externally visible portion of this library.
-    var Live = {
-        live: function(input, callback, context, matching) {
-            var selectors = jQuery.trim(input).split(' ');
-            if(selectors.length == 0) return;
+            var rest = null;
+            if(tokens.length > 1) {
+                rest = _.chain(tokens).rest().reduce(function(memo, token) {
+                    return memo ? memo + ' ' + token : token;
+                }).value();
+            }
 
-            // Detect existing matches, as well as matches that will be added.
-            // The callbacks should differ depending on if this is the last selector. 
+            var intermediate = _.bind(intermediate_callback, this, rest, callback, context, matching);
+            if(first === '*') {
+                _this.each(intermediate);
+            } else if(_this.get(first)) {
+                intermediate(_this.get(first));
+            }
 
-            // This is where we do a bit of "patient recursion"...replace the callback
-            // with a function that calls live on the children with the tail of the selection
-            // string...and passes along the original callback so that it will be called when 
-            // an attribute/model matches the final selector token.
-            if(selectors.length > 1) {
-                var finisher = callback;
-                callback = function(elem) {
-                    // Check if the argument passed to the callback is a Backbone object. If not, 
-                    // then it obviously won't have any children that match our selector.
-                    var index = 0;
-                    if(elem[index] instanceof Backbone.Model || elem[index] instanceof Backbone.Collection) {
-                        var tmp = _(selectors).tail().reduce(function(memo, part) { return memo + ' ' + part; });
-                        elem[index].live.call(elem[index], tmp, finisher, context, elem);
-                    } else {
-                        debugger;
+            var event_name, event_callback, event_context;
+            if(first === '*') {
+                event_name = 'add';
+                event_callback = intermediate;
+                event_context = _this;
+            } else {
+                event_name = 'add';
+                event_callback = function(model) {
+                    if(model.id === first) {
+                        intermediate(model);
                     }
                 };
-            } else {
-                var finisher = callback;
-                callback = function() {
-                    finisher.apply(this, arguments[0]);
-                }
+                event_context = _this;
             }
+            //this is the only bound event on the object.
+            //die needs only clean this callback up
+            _this.on(event_name, event_callback, event_context);
+        },
 
-            var selector = _(selectors).first();
-            if(this instanceof Backbone.Model) {
-                handle_model.call(this, selector, callback, context, matching || []);
-            } else if(this instanceof Backbone.Collection) {
-                handle_collection.call(this, selector, callback, context, matching || []);
-            } else {
-                debugger;
-            }
+        die: function(selectors, callback, context) {
+
         }
-    };
+    });
 
-    // Attach the 'live' function
-    _.extend(Backbone.Model.prototype, Live);
-    _.extend(Backbone.Collection.prototype, Live);
+    _.extend(Backbone.Model.prototype, {
+        /**
+            @param selectors - a space delimited string of attribute keys/model ids to match
+            example: 'torrent * file * properties streaming_url'
+            @param callback - the callback to be called with the matching values, in reverse order
+            example: live('torrent * file * properties streaming_url', function(streaming_url, properties, file, file_list...)...
+            @param context - the this pointer in the callback
+            @param matching - not for external use...it is used to collect the callback arguments
+        **/
+        live: function(selectors, callback, context, matching) {
+            var _this = this;
+            var tokens = selectors.split(' ');
+
+            var first = null;
+            if(tokens.length > 0) {
+                first = _.first(tokens);
+            }
+
+            var rest = null;
+            if(tokens.length > 1) {
+                rest = _.chain(tokens).rest().reduce(function(memo, token) { 
+                    return memo ? memo + ' ' + token : token;
+                }).value();
+            }
+
+            var intermediate = _.bind(intermediate_callback, this, rest, callback, context, matching);
+            if(first === '*') {
+                _.each(_this.toJSON(), intermediate);
+            } else if(_this.has(first)) {
+                intermediate(_this.get(first));
+            }
+
+            var event_name, event_callback, event_context;
+            if(first === '*') {
+                event_name = 'change';
+                event_callback = function() {
+                    _.each(_this.changedAttributes(), function(value, key) {
+                        if(typeof _this.previous(key) === 'undefined') 
+                            intermediate(value);
+                    });
+                };
+                event_context = _this;
+            } else {
+                event_name = 'change:' + first;
+                event_callback = function() {
+                    if(typeof _this.previous(first) === 'undefined') {
+                        //we have a new attribute!
+                        var value = _this.get(first);
+                        intermediate(value);
+                    }
+                };
+                event_context = _this;
+            }
+
+            //this is the only bound event on the object.
+            //die needs only clean this callback up
+            _this.on(event_name, event_callback, event_context);
+        },
+
+        die: function(selectors, callback, context) {
+
+        }
+    });
 }).call(this);
--- spec/javascripts/specs/backbrace_spec.js
+++ spec/javascripts/specs/backbrace_spec.js
@@ -1,220 +1,238 @@
 (function() {
 	describe('Tests', function() {
-		it('makes live available on Backbone.Models', function() {
-			var model = new Backbone.Model;
-			expect(model.live).toBeDefined();
-		});
-		it('makes live available on Backbone.Collection', function() {
-			var collection = new Backbone.Collection;
-			expect(collection.live).toBeDefined();
-		});
-		it('calls a callback for a static selector containing only models that are added after live call', function() {
-			runs(function() {
-				var callback = jasmine.createSpy();
+		describe('Live Tests', function() {
+			it('makes live available on Backbone.Models', function() {
 				var model = new Backbone.Model;
-				model.live('a b c d', callback);
-
-				model.set('a', new Backbone.Model);
-				model.get('a').set('b', new Backbone.Model);
-				model.get('a').get('b').set('c', new Backbone.Model);
-				model.get('a').get('b').get('c').set('d', new Backbone.Model);
-
-				expect(callback).toHaveBeenCalled();
+				expect(model.live).toBeDefined();
 			});
-		});
-		it('calls a callback for a static selector containing only models that are added before live call', function() {
-			runs(function() {
-				var callback = jasmine.createSpy();
-				var model = new Backbone.Model;
-
-				model.set('a', new Backbone.Model);
-				model.get('a').set('b', new Backbone.Model);
-				model.get('a').get('b').set('c', new Backbone.Model);
-				model.get('a').get('b').get('c').set('d', new Backbone.Model);
-
-				model.live('a b c d', callback);
-				expect(callback).toHaveBeenCalled();
+			it('makes live available on Backbone.Collection', function() {
+				var collection = new Backbone.Collection;
+				expect(collection.live).toBeDefined();
 			});
-		});
-		it('calls a callback for a static selector containing models and collections that are added after live call', function() {
-			runs(function() {
+			it('calls a callback for a static selector containing only models that are added after live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+					model.live('a b c d', function(d, c, b, a) {
+						expect(d.id).toEqual('d');
+						expect(c.id).toEqual('c');
+						expect(b.id).toEqual('b');
+						expect(a.id).toEqual('a');
+						callback();
+					});
+
+					model.set('a', new Backbone.Model({id: 'a'}));
+					model.get('a').set('b', new Backbone.Model({id: 'b'}));
+					model.get('a').get('b').set('c', new Backbone.Model({id: 'c'}));
+					model.get('a').get('b').get('c').set('d', new Backbone.Model({id: 'd'}));
+
+					expect(callback).toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for a static selector containing only models that are added before live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+
+					model.set('a', new Backbone.Model);
+					model.get('a').set('b', new Backbone.Model);
+					model.get('a').get('b').set('c', new Backbone.Model);
+					model.get('a').get('b').get('c').set('d', new Backbone.Model);
+
+					model.live('a b c d', callback);
+					expect(callback).toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for a static selector containing models and collections that are added after live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+					model.live('a b c d', callback);
+
+					model.set('a', new Backbone.Collection);
+					model.get('a').add(new Backbone.Model({id: 'b'}));
+					model.get('a').get('b').set('c', new Backbone.Collection);
+					model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
+
+					expect(callback).toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for a static selector containing models and collections that are added before live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+
+					model.set('a', new Backbone.Collection);
+					model.get('a').add(new Backbone.Model({id: 'b'}));
+					model.get('a').get('b').set('c', new Backbone.Collection);
+					model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
+
+					model.live('a b c d', callback);
+					expect(callback).toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for a wildcard selector containing models and collections that are added after live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+					model.live('a * * name', callback);
+
+					model.set('a', new Backbone.Collection);
+					model.get('a').add(new Backbone.Model({id: 'b'}));
+					model.get('a').add(new Backbone.Model({id: 'c'}));
+					model.get('a').get('b').set('d', new Backbone.Model);
+					model.get('a').get('c').set('e', new Backbone.Model);
+
+					model.get('a').get('b').get('d').set('name', new Backbone.Model);
+					model.get('a').get('c').get('e').set('name', new Backbone.Model);
+
+					expect(callback).toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for a wildcard selector containing models and collections that are added before live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+
+					model.set('a', new Backbone.Collection);
+					model.get('a').add(new Backbone.Model({id: 'b'}));
+					model.get('a').get('b').set('c', new Backbone.Collection);
+					model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
+
+					model.live('a b c d', callback);
+					expect(callback).toHaveBeenCalled();
+				});
+			});		
+			it('does not call a callback when not all models are added before live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+
+					model.set('a', new Backbone.Model);
+					model.get('a').set('b', new Backbone.Model);
+					model.get('a').get('b').set('c', new Backbone.Model);
+
+					model.live('a b c d', callback);
+					expect(callback).not.toHaveBeenCalled();
+				});
+			});
+			it('handles the * example', function() {
 				var callback = jasmine.createSpy();
 				var model = new Backbone.Model;
-				model.live('a b c d', callback);
+				model.live('a * c *', callback);
 
 				model.set('a', new Backbone.Collection);
 				model.get('a').add(new Backbone.Model({id: 'b'}));
 				model.get('a').get('b').set('c', new Backbone.Collection);
 				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
-
-				expect(callback).toHaveBeenCalled();
+				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'e'}));
+				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'f'}));
+				expect(callback.callCount).toEqual(3);
 			});
-		});
-		it('calls a callback for a static selector containing models and collections that are added before live call', function() {
-			runs(function() {
+			it('handles the * name example', function() {
 				var callback = jasmine.createSpy();
+				var collection = new Backbone.Collection;
+				collection.live('* name', callback);
+
+				var daniel = new Backbone.Model({name: 'Daniel'});
+				collection.add(daniel);
+				expect(callback).toHaveBeenCalledWith('Daniel', daniel);	
+
+				var robert = new Backbone.Model({name: 'Robert'});
+				collection.add(robert);
+				expect(callback).toHaveBeenCalledWith('Robert', robert);
+
+				var mary = new Backbone.Model({name: 'Mary'});
+				collection.add(mary);
+				expect(callback).toHaveBeenCalledWith('Mary', mary);
+
+				expect(callback.callCount).toEqual(3);
+				//The following works too!
+				var person = new Backbone.Model();
+				collection.add(person);
+				person.set('name', 'Patrick');
+				expect(callback.callCount).toEqual(4);
+			});
+			it('does not call a callback when not all models are added after live call', function() {
+				runs(function() {
+					var callback = jasmine.createSpy();
+					var model = new Backbone.Model;
+					model.live('a b c d', callback);
+
+					model.set('a', new Backbone.Model);
+					model.get('a').set('b', new Backbone.Model);
+					model.get('a').get('b').set('c', new Backbone.Model);
+
+					expect(callback).not.toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for purely model selector with the correct context', function() {
+				runs(function() {
+					var context = 'hi i am a context';
+					var spy = jasmine.createSpy();
+					var callback = function() {
+						expect(this === context);
+						spy();
+					};
+					var model = new Backbone.Model;
+
+					model.set('a', new Backbone.Model);
+					model.get('a').set('b', new Backbone.Model);
+					model.get('a').get('b').set('c', new Backbone.Model);
+					model.get('a').get('b').get('c').set('d', new Backbone.Model);
+
+					model.live('a b c d', callback, context);
+					expect(spy).toHaveBeenCalled();
+				});
+			});
+			it('calls a callback for mixed model/collection selector with the correct context', function() {
+				runs(function() {
+					var context = 'hi i am a context';
+					var spy = jasmine.createSpy();
+					var callback = function() {
+						expect(this === context);
+						spy();
+					};
+					var model = new Backbone.Model;
+					model.live('a b c d', callback, context);
+
+					model.set('a', new Backbone.Collection);
+					model.get('a').add(new Backbone.Model({id: 'b'}));
+					model.get('a').get('b').set('c', new Backbone.Collection);
+					model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
+
+					expect(spy).toHaveBeenCalled();
+				});
+			});		
+			it('calls a callback for mixed model/collection selector with the correct tree of objects', function() {
+				runs(function() {
+					var context = 'hi i am a context';
+					var spy = jasmine.createSpy();
+					var model = new Backbone.Model;
+					model.live('a b c d', spy, context);
+
+					var a = new Backbone.Collection;
+					var b = new Backbone.Model({id: 'b'});
+					var c = new Backbone.Collection;
+					var d = new Backbone.Model({id: 'd'});
+					model.set('a', a);
+					model.get('a').add(b);
+					model.get('a').get('b').set('c', c);
+					model.get('a').get('b').get('c').add(d);
+
+					expect(spy).toHaveBeenCalledWith(d, c, b, a);
+				});
+			});			
+		});
+		describe('Die Tests', function() {
+			it('makes die available on Backbone.Models', function() {
 				var model = new Backbone.Model;
-
-				model.set('a', new Backbone.Collection);
-				model.get('a').add(new Backbone.Model({id: 'b'}));
-				model.get('a').get('b').set('c', new Backbone.Collection);
-				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
-
-				model.live('a b c d', callback);
-				expect(callback).toHaveBeenCalled();
+				expect(model.die).toBeDefined();
+			});
+			it('makes die available on Backbone.Collection', function() {
+				var collection = new Backbone.Collection;
+				expect(collection.die).toBeDefined();
 			});
 		});
-		it('calls a callback for a wildcard selector containing models and collections that are added after live call', function() {
-			runs(function() {
-				var callback = jasmine.createSpy();
-				var model = new Backbone.Model;
-				model.live('a * * name', callback);
-
-				model.set('a', new Backbone.Collection);
-				model.get('a').add(new Backbone.Model({id: 'b'}));
-				model.get('a').add(new Backbone.Model({id: 'c'}));
-				model.get('a').get('b').set('d', new Backbone.Model);
-				model.get('a').get('c').set('e', new Backbone.Model);
-
-				model.get('a').get('b').get('d').set('name', new Backbone.Model);
-				model.get('a').get('c').get('e').set('name', new Backbone.Model);
-
-				expect(callback).toHaveBeenCalled();
-			});
-		});
-		it('calls a callback for a wildcard selector containing models and collections that are added before live call', function() {
-			runs(function() {
-				var callback = jasmine.createSpy();
-				var model = new Backbone.Model;
-
-				model.set('a', new Backbone.Collection);
-				model.get('a').add(new Backbone.Model({id: 'b'}));
-				model.get('a').get('b').set('c', new Backbone.Collection);
-				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
-
-				model.live('a b c d', callback);
-				expect(callback).toHaveBeenCalled();
-			});
-		});		
-		it('does not call a callback when not all models are added before live call', function() {
-			runs(function() {
-				var callback = jasmine.createSpy();
-				var model = new Backbone.Model;
-
-				model.set('a', new Backbone.Model);
-				model.get('a').set('b', new Backbone.Model);
-				model.get('a').get('b').set('c', new Backbone.Model);
-
-				model.live('a b c d', callback);
-				expect(callback).not.toHaveBeenCalled();
-			});
-		});
-		it('handles the * example', function() {
-			var callback = jasmine.createSpy();
-			var model = new Backbone.Model;
-			model.live('a * c *', callback);
-
-			model.set('a', new Backbone.Collection);
-			model.get('a').add(new Backbone.Model({id: 'b'}));
-			model.get('a').get('b').set('c', new Backbone.Collection);
-			model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
-			model.get('a').get('b').get('c').add(new Backbone.Model({id: 'e'}));
-			model.get('a').get('b').get('c').add(new Backbone.Model({id: 'f'}));
-			expect(callback.callCount).toEqual(3);
-		});
-		it('handles the * name example', function() {
-			var callback = jasmine.createSpy();
-			var collection = new Backbone.Collection;
-			collection.live('* name', callback);
-
-			var daniel = new Backbone.Model({name: 'Daniel'});
-			collection.add(daniel);
-			expect(callback).toHaveBeenCalledWith('Daniel', daniel);	
-
-			var robert = new Backbone.Model({name: 'Robert'});
-			collection.add(robert);
-			expect(callback).toHaveBeenCalledWith('Robert', robert);
-
-			var mary = new Backbone.Model({name: 'Mary'});
-			collection.add(mary);
-			expect(callback).toHaveBeenCalledWith('Mary', mary);
-
-			expect(callback.callCount).toEqual(3);
-			//The following works too!
-			var person = new Backbone.Model();
-			collection.add(person);
-			person.set('name', 'Patrick');
-			expect(callback.callCount).toEqual(4);
-		});
-		it('does not call a callback when not all models are added after live call', function() {
-			runs(function() {
-				var callback = jasmine.createSpy();
-				var model = new Backbone.Model;
-				model.live('a b c d', callback);
-
-				model.set('a', new Backbone.Model);
-				model.get('a').set('b', new Backbone.Model);
-				model.get('a').get('b').set('c', new Backbone.Model);
-
-				expect(callback).not.toHaveBeenCalled();
-			});
-		});
-		it('calls a callback for purely model selector with the correct context', function() {
-			runs(function() {
-				var context = 'hi i am a context';
-				var spy = jasmine.createSpy();
-				var callback = function() {
-					expect(this === context);
-					spy();
-				};
-				var model = new Backbone.Model;
-
-				model.set('a', new Backbone.Model);
-				model.get('a').set('b', new Backbone.Model);
-				model.get('a').get('b').set('c', new Backbone.Model);
-				model.get('a').get('b').get('c').set('d', new Backbone.Model);
-
-				model.live('a b c d', callback, context);
-				expect(spy).toHaveBeenCalled();
-			});
-		});
-		it('calls a callback for mixed model/collection selector with the correct context', function() {
-			runs(function() {
-				var context = 'hi i am a context';
-				var spy = jasmine.createSpy();
-				var callback = function() {
-					expect(this === context);
-					spy();
-				};
-				var model = new Backbone.Model;
-				model.live('a b c d', callback, context);
-
-				model.set('a', new Backbone.Collection);
-				model.get('a').add(new Backbone.Model({id: 'b'}));
-				model.get('a').get('b').set('c', new Backbone.Collection);
-				model.get('a').get('b').get('c').add(new Backbone.Model({id: 'd'}));
-
-				expect(spy).toHaveBeenCalled();
-			});
-		});		
-		it('calls a callback for mixed model/collection selector with the correct tree of objects', function() {
-			runs(function() {
-				var context = 'hi i am a context';
-				var spy = jasmine.createSpy();
-				var model = new Backbone.Model;
-				model.live('a b c d', spy, context);
-
-				var a = new Backbone.Collection;
-				var b = new Backbone.Model({id: 'b'});
-				var c = new Backbone.Collection;
-				var d = new Backbone.Model({id: 'd'});
-				model.set('a', a);
-				model.get('a').add(b);
-				model.get('a').get('b').set('c', c);
-				model.get('a').get('b').get('c').add(d);
-
-				expect(spy).toHaveBeenCalledWith(d, c, b, a);
-			});
-		});		
 	});
 }).call(this);
